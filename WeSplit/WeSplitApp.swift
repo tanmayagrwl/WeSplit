85c8d57d3e8673738bf24377e576bb0dc7136142
tanmayagrwl <tanmayhp78@gmail.com>
2024-11-13 14:06:08 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tanmay Agrawal on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
